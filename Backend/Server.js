8038be2fdcedbbd86f8eb655f627d7e08de66c9c
shubhampund9325 <pundshubham9767@gmail.com>
2025-01-14 21:47:17 +0530

    Product route completed

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-// import productRoutes from "./routes/product.route.js";
+import productRoutes from "./routes/product.route.js";
 // import cartRoutes from "./routes/cart.route.js";
 // import couponRoutes from "./routes/coupon.route.js";
 // import paymentRoutes from "./routes/payment.route.js";
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 // app.use("/api/cart", cartRoutes);
 // app.use("/api/coupons", couponRoutes);
 // app.use("/api/payments", paymentRoutes);
